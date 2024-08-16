e620b87c3c0660950beed9e4a780371519b73eb4
cameronmcginley <mcginleycameron@gmail.com>
2024-08-16 16:51:10 -0700

    Test

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      crypto: require.resolve("crypto-browserify"),
+    };
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      })
+    );
+    return config;
   },
 };
 
