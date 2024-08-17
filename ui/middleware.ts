69c0fe747dd32cb4df448155211fb1b18367e366
cameronmcginley <mcginleycameron@gmail.com>
2024-08-16 20:40:48 -0700

    Remove lib from middleware

--- ui/middleware.ts
+++ ui/middleware.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
-import { SIGN_IN_URL } from "@/lib/constants";
 
 export default withAuth(function middleware(req) {
   if (!req.nextauth.token) {
-    return NextResponse.redirect(new URL(SIGN_IN_URL, req.url).toString());
+    return NextResponse.redirect(new URL("/sign-in", req.url).toString());
   }
 });
 
