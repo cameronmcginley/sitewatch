63e343136a843d43732bf59a172191f4e509b6d8
cameronmcginley <mcginleycameron@gmail.com>
2024-08-16 19:12:52 -0700

    Test

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,10 +1,8 @@
-import webpack from "webpack";
-
 /** @type {import('next').NextConfig} */ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
       // Provide a fallback for Node.js built-ins like crypto, if needed
       config.resolve.fallback = {
@@ -12,14 +10,14 @@ import webpack from "webpack";
         crypto: require.resolve("crypto-browserify"),
         process: require.resolve("process/browser"),
       };
-    }
 
-    // Add ProvidePlugin to inject process where needed
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-      })
-    );
+      // Add ProvidePlugin to inject process where needed
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+        })
+      );
+    }
 
     return config;
   },
